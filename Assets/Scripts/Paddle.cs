7960e13ca81ff6c2ee4e058b246db0c298290a81
Yue Liu <liuer99cn@aliyun.com>
2016-11-19 15:01:00 +0800

    feat: Add auto test on paddle
    feat: Add auto test on paddle

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,20 +1,45 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Paddle : MonoBehaviour {
+    public bool autoPlay = false;
+
+    private Ball ball;
 
 	// Use this for initialization
 	void Start () {
-	
+        ball = GameObject.FindObjectOfType<Ball>();
+        print(ball);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (!autoPlay)
+        {
+            MoveWithMouse();
+        }else
+        {
+            AutoPlay();
+        }
+	
+	}
+
+    void MoveWithMouse()
+    {
         Vector3 paddlePos = new Vector3(0.5f, 0.22f, 0f);
         float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
         //print(mousePosInBlocks);
         paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
         this.transform.position = paddlePos;
-	
-	}
+    }
+
+    void AutoPlay()
+    {
+        Vector3 paddlePos = new Vector3(0.5f, 0.22f, 0f);
+        Vector3 ballPos = ball.transform.position;
+        //print(mousePosInBlocks);
+        paddlePos.x = Mathf.Clamp(ballPos.x, 0.5f, 15.5f);
+        this.transform.position = paddlePos;
+
+    }
 }
