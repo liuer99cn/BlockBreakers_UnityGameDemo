eaaf11cc03995c55334e42377f3f422870d7ff81
Yue Liu <liuer99cn@aliyun.com>
2016-11-20 16:29:56 +0800

    docs: change output message
    docs: change output message

--- Assets/Scripts/LooseCollider.cs
+++ Assets/Scripts/LooseCollider.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class LooseCollider : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D trigger) {
-		print ("Trigger");
+		print ("Trigger on");
 	}
 
 	void OnCollisionEnter2D (Collision2D collision) {
